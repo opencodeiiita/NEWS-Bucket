80008bf619ef92a955ad8b30802d58252c1cfeac
Cybertron-Avneesh <iib2019010@iiita.ac.in>
2020-01-22 00:38:48 +0530

    Added functionality to fetch NEWS from newsAPI.json

--- app/src/main/java/com/kinshuu/news_bucket/MainActivity.kt
+++ app/src/main/java/com/kinshuu/news_bucket/MainActivity.kt
@@ -3,52 +3,62 @@ package com.kinshuu.news_bucket
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.ArrayAdapter
-import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
-import kotlinx.android.synthetic.main.activity_detailnews.*
 import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONArray
+import java.io.IOException
+import java.io.InputStream
 
 class MainActivity : AppCompatActivity() {
 
+    var news_heading = arrayListOf<String>()
+    var news_description = arrayListOf<String>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val news_heading = arrayOf<String>("OpenCode'20 began on 17.Jan.2020",
-                "This is dummy news.Pay no attention to it.",
-                "Cows lose their jobs as milk prices drop.",
-                "Miracle cure kills fifth patient",
-                "Most Earthquake Damage is Caused by Shaking",
-                "Bugs flying around with wings are flying bugs",
-                "Farmer using cannon to protect watermelons",
-                "Diana was still alive hours before she died")
+        read_json()
 
-        val news_description = arrayOf<String>(
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+        //var fabBtn: View = findViewById(R.id.fabBtn)
 
-        listView.adapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,news_heading);
-
-        val fabBtn: View = findViewById(R.id.fabBtn)
         fabBtn.setOnClickListener { view ->
             Snackbar.make(view, "Fetching Latest News", Snackbar.LENGTH_SHORT)
                 .setAction("Action", null)
                 .show()
         }
+    }
+    fun read_json()
+    {
+        var json : String? = null
+        try {
+            var inputStream: InputStream = assets.open("newsAPI.json")
 
-        listView.setOnItemClickListener { parent, view, position, id ->
-            val intent = Intent(this, detailnews::class.java)
-            intent.putExtra("Heading",news_heading[position])
-            intent.putExtra("Description",news_description[position])
-            startActivity(intent)
+            json = inputStream.bufferedReader().use { it.readText() }
+            var jsonarr = JSONArray(json)
+
+            for(i in 0..jsonarr.length()-1)
+            {
+                var jsonobj = jsonarr.getJSONObject(i)
+                news_heading.add(jsonobj.getString("title"))
+                news_description.add(jsonobj.getString("description"))
+            }
+
+            var listAdapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,news_heading)
+            listView.adapter = listAdapter
+
+            listView.setOnItemClickListener { parent, view, position, id ->
+                var intent = Intent(this, detailnews::class.java)
+                intent.putExtra("Heading", news_heading[position])
+                intent.putExtra("Description", news_description[position])
+                startActivity(intent)
+            }
         }
+
+        catch (e : IOException)
+        {
+
+        }
+
     }
 }
